7c5a52e2eea93bd40e095b4b1ca910b73170bb96
JavierAmaya <photojavhn@gmail.com>
2020-05-03 21:25:21 -0600

    adding react packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import "./css/main.css";
-import Course from "./views/course"
+import Course from "./views/course";
 
 const App:React.FC = () => (
-  <Course></Course>
+  <BrowserRouter>
+    <Switch>
+      <Route path="/course/:id" exact component ={Course} />
+    </Switch>
+  </BrowserRouter>
 )
 
 export default App;
--- src/views/course.tsx
+++ src/views/course.tsx
@@ -1,14 +1,41 @@
-import React,{useState} from "react";
+import React,{useState, useEffect} from "react";
+import {useParams} from "react-router-dom";
 import Header,{} from "../components/header";
 import Card, {} from "../components/cards";
 
-import {information_service} from "../fakeservices/information"
+import {information_service} from "../fakeservices/information";
+import {courses} from "../fakeservices/courses";
+
+
+interface IDataElement{
+    id:number,
+    name:string
+}
+
+interface IReturn{
+    success: boolean
+    name:string
+}
+
+const getCourse = (id:number) : IReturn => {
+    let result : IReturn = { success: false , name:"CURSO NO ENCONTRADO"}
+    courses.forEach(function(value:IDataElement) {
+        if (value.id === id) {
+            result.success = true;
+            result.name = value.name;
+        }  
+    });
+    return result
+}
 
 const Course:React.FC = () => {
     const [showConfirmation,setShowConfirmation] = useState(true);
     const [action, setAction]= useState(false);
     const [question,setQuestion] = useState(false);
     const [sad,setSad] = useState(false);
+    const [titleH, setTitleH] = useState({success:false, name:"CURSO NO ENCONTRADO"});
+
+    const {id} = useParams();
 
 
     const accept = ()=>{
@@ -30,15 +57,21 @@ const Course:React.FC = () => {
         setAction(false);
         setQuestion(false);
         setSad(false);
-    }
+    } 
+
+    useEffect( () =>{
+        const n : number = parseInt(id!);
+        const t:IReturn = getCourse(n);
+        setTitleH(t)
+    },[id])
 
     
 
     return(
         <div>
-          <Header title="Base de Datos" show_button={question} again={again}/>
+          <Header title={titleH.name} show_button={question} again={again}/>
           <Card
-             open={showConfirmation}
+             open={titleH.success && showConfirmation}
              title={information_service[0].title}
              description={information_service[0].description}
              question={information_service[0].pregunta}
